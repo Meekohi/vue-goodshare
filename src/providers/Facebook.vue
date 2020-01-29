115c4828a459bf6b99ba2123a7450478f9c87562
Michael Holroyd <meekohi@gmail.com>
2020-01-29 13:08:07 -0500

    fontello

--- src/providers/Facebook.vue
+++ src/providers/Facebook.vue
@@ -147,13 +147,13 @@ export default {
 <style scoped lang="scss">
 // Fontello
 @font-face {
-  font-family: "Fontello";
-  src: url("../fonts/fontello.eot?26533562");
-  src: url("../fonts/fontello.eot?26533562#iefix") format("embedded-opentype"),
-    url("../fonts/fontello.woff2?26533562") format("woff2"),
-    url("../fonts/fontello.woff?26533562") format("woff"),
-    url("../fonts/fontello.ttf?26533562") format("truetype"),
-    url("../fonts/fontello.svg?26533562#fontello") format("svg");
+  font-family: 'Fontello';
+  src: url('https://s3.amazonaws.com/assets.scenethink.com/fonts/fontello.eot?88483298');
+  src: url('https://s3.amazonaws.com/assets.scenethink.com/fonts/fontello.eot?88483298#iefix') format('embedded-opentype'),
+  url('https://s3.amazonaws.com/assets.scenethink.com/fonts/fontello.woff2?88483298') format('woff2'),
+  url('https://s3.amazonaws.com/assets.scenethink.com/fonts/fontello.woff?88483298') format('woff'),
+  url('https://s3.amazonaws.com/assets.scenethink.com/fonts/fontello.ttf?88483298') format('truetype'),
+  url('https://s3.amazonaws.com/assets.scenethink.com/fonts/fontello.svg?88483298#fontello') format('svg');
   font-weight: normal;
   font-style: normal;
 }
